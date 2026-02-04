869c2da3b36897204de2f865ee38dd88242ccf21
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2026-02-04 15:55:42 +0800

    feat: establish a GitHub Actions CI/CD pipeline for testing, building, and deployment. and fix triple processing tailwind conflict

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
-             refresh: [
-                    'resources/views/**', // Watch all Blade files in resources/views
-                    'routes/**',          // Watch your route files as well
-                ],
+            refresh: [
+                'resources/views/**', // Watch all Blade files in resources/views
+                'routes/**',          // Watch your route files as well
+            ],
         }),
-        tailwindcss(),
         react(),
     ],
 });
