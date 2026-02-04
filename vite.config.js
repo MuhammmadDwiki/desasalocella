de5c0142189c6180254e36414169d8e878800da4
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2026-02-04 18:37:44 +0800

    fix: error testing in ci/cd pipeline

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx'
+            ],
             refresh: [
                 'resources/views/**', // Watch all Blade files in resources/views
                 'routes/**',          // Watch your route files as well
